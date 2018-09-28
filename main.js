f29fe54f1d078a5e9e09947ab4cbe5bf9b4ae9ec
Kar.Rob <robindebx@gmail.com>
2018-09-28 22:16:49 +0200

    ajout de la popup

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+window.setTimeout("confirmation()",10000);function confirmation() {
+var answer = confirm("Hey le site xxxvidsxxx est trop bien. Clique sur OK pour y accéder, tu ne le regretteras pas")
+if (answer) {
+	window.location = " https://www.youtube.com/watch?v=dQw4w9WgXcQ.";
+    }}
